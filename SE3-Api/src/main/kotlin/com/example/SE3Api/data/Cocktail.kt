7a49f14b01a6aff9fe7dae407487f40a1afce4e4
JonasFichtnerPflaum <jonas.fichtnerpflaum@gmail.com>
2023-05-14 15:53:47 +0200

    test!

--- SE3-Api/src/main/kotlin/com/example/SE3Api/data/Cocktail.kt
+++ SE3-Api/src/main/kotlin/com/example/SE3Api/data/Cocktail.kt
@@ -1,11 +1,17 @@
 package com.example.SE3Api.data
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 
-data class Cocktail(
-    @Id
-    val id: Long = 0,
-    val name: String,
-    val ingredients: String,
-    val instructions: String
+@Document("cocktail")
+data class Cocktail (
+
+        @Id
+        var id: String,
+        var name: String,
+        var ingredients: String,
+        var difficulty: String,
+        var alcoholic: Boolean,
+        var taste: String
+
 )
