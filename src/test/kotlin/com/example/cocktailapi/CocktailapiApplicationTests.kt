ec775816dac994cc6e43c486f5dfd3343287f9f7
JonasFichtnerPflaum <98275550+JonasFichtnerPflaum@users.noreply.github.com>
2023-05-26 11:29:12 +0200

    Add files via upload

---
+++ src/test/kotlin/com/example/cocktailapi/CocktailapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cocktailapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CocktailapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
